878244a6e7da097d0951bd02f035663b607f838b
MakotoTaguchi <taguchimakoto@icloud.com>
2022-10-13 18:09:35 +0900

    Escape

---
+++ Tutorial/EscapeTest.java
@@ -0,0 +1,8 @@
+public class EscapeTest {
+    public static void main(String[] args) {
+        System.out.println("Double quote(\"\") is displayed.");
+        System.out.println("\\ is displayed.");
+        System.out.println("Hello!\nWorld!!");
+        System.out.println("Hello!\tWorld!!");
+    }
+}
