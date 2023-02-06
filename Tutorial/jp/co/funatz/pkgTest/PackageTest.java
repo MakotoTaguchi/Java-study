5d4265c3ae8a9bfe3d8d4d3d319b91173ad3b7e0
MakotoTaguchi <taguchimakoto@icloud.com>
2023-02-06 20:53:38 +0900

    package

--- Tutorial/jp/co/funatz/pkgTest/PackageTest.java
+++ Tutorial/jp/co/funatz/pkgTest/PackageTest.java
@@ -1,11 +1,15 @@
 package jp.co.funatz.pkgTest;
 
 import jp.co.funatz.pkgTest.a.PackageA;
+// import jp.co.funatz.pkgTest.a.PackageZ;
 import jp.co.funatz.pkgTest.b.PackageB;
+// import jp.co.funatz.pkgTest.b.PackageZ;
 
 public class PackageTest {
     public static void main(String[] args) {
         PackageA.a();
         PackageB.b();
+        jp.co.funatz.pkgTest.a.PackageZ.z();
+        jp.co.funatz.pkgTest.b.PackageZ.z();
     }
 }
